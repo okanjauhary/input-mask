90835fa06bba4b909a1301c1612a49d6a98f2605
jauhary <ms.jauhary@gmail.com>
2019-05-14 10:16:51 +0700

    add methods to listen event blur and focus, add classes disable, active and focus

--- src/app/masking.js
+++ src/app/masking.js
@@ -18,13 +18,26 @@ const Masking = (() => {
             await this.initChildInput()
             this.listenEvent()
         }
+
+        disabledElement(el){
+            el.setAttribute('disabled', true)
+            el.classList.add('input-mask--is-disabled')
+            el.classList.remove('input-mask--is-active')
+        }
+
+        enableElement(el){
+            el.removeAttribute('disabled')
+            el.classList.add('input-mask--is-active')
+            el.classList.remove('input-mask--is-disabled')
+        }
     
         initChildInput(){
             return new Promise(resolve => {
                 for(let i=0; i < this.rules.length; i++){
                     let child = document.createElement('input')
-                    child.setAttribute('class', `msj-input-mask__content input-mask-${i+1}--${this.rules[i]}`)
-                    if(i > 0) child.setAttribute('disabled', true)
+                    child.setAttribute('class', `msj-input-mask__item input-mask-${i+1}--${this.rules[i]}`)
+                    if(i > 0) this.disabledElement(child)
+                    else this.enableElement(child)
                     this.el.appendChild(child)
                 }
                 resolve(true)
@@ -32,33 +45,51 @@ const Masking = (() => {
         }
     
         listenEvent(){
-            let input = this.el.querySelectorAll('.msj-input-mask__content')
+            let input = this.el.querySelectorAll('.msj-input-mask__item')
             for(let i=0; i < input.length; i++){
-                input[i].addEventListener('keyup', e => {
-                    let val = clear(e.target.value)
-                    if(/* backspace */e.keyCode == 8 ){
-                        this.whenDelete(input, val, i)
-                    }else{
-                        if(/* checking length of value */ val.length > this.rules[i]){
-                            e.target.value = val.slice(0, this.rules[i])
-                            if(/* checking not last input */ i < input.length-1){
-                                input[i+1].removeAttribute('disabled')
-                                input[i+1].focus()
-                                if(input[i+1].value.length < this.rules[i+1]){
-                                    input[i+1].value += val.slice(-1)
-                                }
-                            }
-                        }else e.target.value = val
-                    }
-                })
+                this.whenKeyup(input, i)
+                this.whenFocus(input, i)
+                this.whenBlur(input, i)
             }
+        }
+
+        whenKeyup(input, i){
+            input[i].addEventListener('keyup', e => {
+                let val = clear(e.target.value)
+                if(/* backspace */e.keyCode == 8 ){
+                    this.whenDelete(input, val, i)
+                }else{
+                    if(/* checking length of value */ val.length > this.rules[i]){
+                        e.target.value = val.slice(0, this.rules[i])
+                        if(/* checking not last input */ i < input.length-1){
+                            this.enableElement(input[i+1])
+                            input[i+1].focus()
+                            if(input[i+1].value.length < this.rules[i+1]){
+                                input[i+1].value += val.slice(-1)
+                            }
+                        }
+                    }else e.target.value = val
+                }
+            })
+        }
+
+        whenFocus(input, i){
+            input[i].addEventListener('focus', e => {
+                e.target.classList.add('input-mask--is-focused')
+            })
+        }
+
+        whenBlur(input, i){
+            input[i].addEventListener('blur', e => {
+                e.target.classList.remove('input-mask--is-focused')
+            })
         }
 
         whenDelete(input, val, index){
             if(/* checking loping not first */index != 0){
                 if(/* checking length is empty */ !val.length){
                     input[index-1].focus()
-                    input[index].setAttribute('disabled', true)
+                    this.disabledElement(input[index])
                 }
             }
         }
